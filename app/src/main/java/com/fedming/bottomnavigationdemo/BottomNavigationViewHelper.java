60f6bbed90aca402a5989e92ab8371975b7de7fc
程龙 <2908574164@qq.com>
2018-10-15 17:10:57 +0800

    First submission

---
+++ app/src/main/java/com/fedming/bottomnavigationdemo/BottomNavigationViewHelper.java
@@ -0,0 +1,35 @@
+package com.fedming.bottomnavigationdemo;
+
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+
+import java.lang.reflect.Field;
+/**
+ * Created by 程大龙 on 2018/10/15.
+ * 利用反射，改变 item 中 mShiftingMode 的值
+ */
+
+
+class BottomNavigationViewHelper {
+
+    static void disableShiftMode(BottomNavigationView navigationView) {
+
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) navigationView.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView itemView = (BottomNavigationItemView) menuView.getChildAt(i);
+                itemView.setShiftingMode(false);
+                itemView.setChecked(itemView.getItemData().isChecked());
+            }
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/fedming/bottomnavigationdemo/HomeActivity.java
@@ -0,0 +1,95 @@
+package com.fedming.bottomnavigationdemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by 程大龙 on 2018/10/15.
+ * HomeActivity 主界面
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private MenuItem menuItem;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        //默认 >3 的选中效果会影响ViewPager的滑动切换时的效果，故利用反射去掉
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.item_news:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.item_lib:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.item_find:
+                                viewPager.setCurrentItem(2);
+                                break;
+                            case R.id.item_more:
+                                viewPager.setCurrentItem(3);
+                                break;
+                                default:
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = bottomNavigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        //禁止ViewPager滑动
+//        viewPager.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                return true;
+//            }
+//        });
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(BaseFragment.newInstance("新闻"));
+        adapter.addFragment(BaseFragment.newInstance("图书"));
+        adapter.addFragment(BaseFragment.newInstance("发现"));
+        adapter.addFragment(BaseFragment.newInstance("更多"));
+        viewPager.setAdapter(adapter);
+    }
+}
